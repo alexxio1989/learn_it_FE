a09b4a40bf2a7ed7ce88f2a573f2c482aaeb2f82
apinna1 <apinna.elis@gmail.com>
2020-10-29 21:35:29 +0100

    'sviluppi'

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
+const path = require('path');
 const express = require('express');
+const port = process.env.PORT || 8080;
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + '/build'));
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
+
+app.listen(port);
+console.log('server_started');
