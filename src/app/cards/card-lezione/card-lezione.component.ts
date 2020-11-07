f62cb9550605bafbec7f108424f3552afe91138d
apinna1 <apinna.elis@gmail.com>
2020-11-07 20:36:26 +0100

    'sviluppi'

--- src/app/cards/card-lezione/card-lezione.component.ts
+++ src/app/cards/card-lezione/card-lezione.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Lezione } from 'src/app/model/Lezione';
+import { isEmptyString } from 'src/app/utils/Util';
 
 @Component({
   selector: 'app-card-lezione',
@@ -10,9 +11,26 @@ export class CardLezioneComponent implements OnInit {
 
   @Input() lezione: Lezione;
 
+  title: string;
+
+  edit: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    if(isEmptyString(this.title)){
+      this.edit = true;
+    }
+  }
+
+  save(){
+    this.lezione.title = this.title;
+    this.edit = false;
+  }
+
+  update(){
+    this.lezione.title = this.title;
+    this.edit = true;
   }
 
 }
