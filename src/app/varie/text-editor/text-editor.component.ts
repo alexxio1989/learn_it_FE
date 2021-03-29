b1fea9d3b5c571e19ad46ba8d98f713969ba0a94
Alessio Pinna <alessiopinna@MacBook-Air-di-Alessio.local>
2021-03-29 09:37:56 +0200

    sviluppi

--- src/app/varie/text-editor/text-editor.component.ts
+++ src/app/varie/text-editor/text-editor.component.ts
@@ -18,11 +18,10 @@ export class TextEditorComponent implements OnInit {
 
   toolbar: Toolbar = [
     ['bold', 'italic'],
-    ['underline', 'strike'],
-    ['code', 'blockquote'],
+    ['underline'],
     ['ordered_list', 'bullet_list'],
     [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-    ['link', 'image'],
+    ['link'],
     ['text_color', 'background_color'],
     ['align_left', 'align_center', 'align_right', 'align_justify'],
   ];
